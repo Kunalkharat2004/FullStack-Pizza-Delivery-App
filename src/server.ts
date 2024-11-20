5460c311312a961877bf5c95bfba42349f08b44e
Kunal <kunalkharat2004@gmail.com>
2024-11-20 22:44:13 +0530

    lint-staged commit 1

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,12 @@ const displayName = (name: string) => {
     console.log(`Hello ${name}`);
     console.log("Kunal");
     console.log("Kharat");
-    
+    const student = {
+        name: "Kunal",
+        age: 25,
+        address: "Pune",
+    };
+    console.log(student.address);
 };
 
 displayName("John Doe");
