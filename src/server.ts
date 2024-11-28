4f30501fd0501b7e0049bec6443552f6bfe7df63
Kunal <kunalkharat2004@gmail.com>
2024-11-28 10:38:56 +0530

    add express

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import app from "./app";
 import config from "./config/config";
 
-console.log(config.PORT, config.NODE_ENV);
+const main = () => {
+    try {
+        const PORT = config.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
+
+main();
