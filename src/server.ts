0fc765c5e48e99a7ff57c415cf01e6cc4de9d82b
Kunal <kunalkharat2004@gmail.com>
2024-11-28 16:17:59 +0530

    add logger winston

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import app from "./app";
 import config from "./config/config";
+import logger from "./config/logger";
 
 const main = () => {
     try {
         const PORT = config.PORT || 3000;
         app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
+            logger.info(`Server is running on port ${PORT}`);
+            logger.error("This is an error log 1");
+            logger.error("This is an error log 2");
+            logger.warn("This is a warning log");
         });
     } catch (err) {
         console.error(err);
